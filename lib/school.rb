350f4ae62d74a7826824ca1214a989a03661c4fd
Daniel Lima <daniel.echuaca.lima@gmail.com>
2019-04-02 15:48:18 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,26 @@
-# code here!
+class School
+  
+  def initialize (name)
+    @name = name
+    @roster = {}
+  end
+
+
+ def roster
+  @roster
+ end
+
+ def add_student (person, grade)
+   @roster[grade] ||= []
+   @roster[grade] << person
+ end
+ 
+ def grade (level)
+   @roster[level]
+ end
+ 
+ def sort
+   @roster.sort 
+ end
+ 
+end
